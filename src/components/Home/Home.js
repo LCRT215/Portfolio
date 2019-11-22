e132e7d2d2d694117748e84349ea4de710bef356
Leslie Thompson <lcrthompson@gmail.com>
2019-11-21 23:31:04 -0500

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,54 @@
 import React from "react";
-import css from "../Home/Home.css";
+import "../Home/Home.css";
 
 function Home() {
   return (
     <div className="home-container">
-      <h1>Hi, I'm Leslie</h1>
-      <p> hi from HOME</p>
-
-      <p>
-        I'm a Full Stack eb Developer based in San Francisco, California. I have
-        a passion for web design and love to create for web and mobile devices
-      </p>
-
-      <div className="what-i-do">
-        <h2>What I do.</h2>
+      <div className="about">
+        <img
+          className="memoji"
+          width="150"
+          height="150"
+          src={require("../images/hand-raised.png")}
+        />
+        <h1>Hi, I'm Leslie!</h1>
         <p>
-          I'm a developer, so I know how to create your website to run across
-          devices using the latest technologies available.
+          I'm a Full Stack Developer based in Philadelphia, PA with experience
+          in creating responsive and interactive websites and apps using the
+          latest technologies available. I have a passion for web design and
+          love to create for web and mobile devices.
+          <hr />
+        </p>
+        I am currently enrolled in Lambda School's 9-month immersive Full Stack
+        Web program, learning to design programs using the industry's latest
+        technologies.
+      </div>
+      <div className="sections one">
+        <h2>Languages</h2>
+        Javascript | Python | HTML | CSS | SQL | noSQL | GraphQL
+      </div>
+      <div className="sections two">
+        <h2> Libraries & Frameworks</h2>
+        React | Redux | Node.js | Express | Knex.js | Firebase | Axios |
+        Postgresql | SCSS | Styled-components | Bootstrap and more...
+      </div>
+      <div className="sections three">
+        <h2> Production Tools</h2>
+        Git | Github | Heroku Notion | Trello
+      </div>
+      <div className="sections four">
+        <h2>Production Strategy</h2>
+        Responsive Design | Feature road mapping | Product boards | User
+        feedback gathering | Story boarding
+      </div>
+      <div>
+        <p>
+          Take a look at some of my recent projects. If you have any questions
+          or would like to chat, please feel free to{" "}
+          <a href="/contact" className="">
+            reach out to me
+          </a>
+          .
         </p>
       </div>
     </div>
