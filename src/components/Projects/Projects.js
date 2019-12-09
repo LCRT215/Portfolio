a964d953ded6c9015a57e07a6258599d6bb0a445
Leslie Thompson <lcrthompson@gmail.com>
2019-12-09 18:41:36 -0500

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
       <div style={{ height: " 300px" }} />
       <ProjectCard
         title="A to Z"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        description="Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       />
     </div>
   );
