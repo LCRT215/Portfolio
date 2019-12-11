8863e201b30c550a3319ee9b335203587a57dbd6
Leslie Thompson <lcrthompson@gmail.com>
2019-12-11 09:55:34 -0500

    trying new project card - simple style

---
+++ src/components/Projects/ProjectCard2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectCard2() {
+  return (
+    <div className="card2-container">
+      <div>Project card 2</div>
+    </div>
+  );
+}
+
+export default ProjectCard2;
