32d0327849f4206d9a257050e5ec5fccf4b3b657
Leslie Thompson <lcrthompson@gmail.com>
2019-12-11 09:50:02 -0500

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,8 +16,9 @@ function Home() {
         <p>
           I'm a Full Stack Developer based in Philadelphia, PA with experience
           in creating responsive and interactive websites and apps using the
-          latest technologies available. I have a passion for web design and
-          love to create for web and mobile devices.
+          latest technologies available. I specialize in Javascript, React, and
+          Node/Express. I have a passion for web design and love to create for
+          web and mobile devices.
           <hr class="home-hr" />
         </p>
         I am currently enrolled in Lambda School's 9-month immersive Full Stack
@@ -28,16 +29,16 @@ function Home() {
       </div>
       <div className="sections one">
         <h2>Languages</h2>
-        Javascript | Python | HTML | CSS | SQL | noSQL | GraphQL
+        Javascript | HTML5 | CSS | SQL | GraphQL
       </div>
       <div className="sections two">
         <h2> Libraries & Frameworks</h2>
-        React | Redux | Node.js | Express | Knex.js | Firebase | Axios |
-        Postgresql | SCSS | Styled-components | Bootstrap and more...
+        React | Redux | Node | Express | Knex | Firebase | Axios | Postgresql |
+        SCSS | Styled-components | Bootstrap and more...
       </div>
       <div className="sections three">
         <h2> Production Tools</h2>
-        Git | Github | Heroku Notion | Trello
+        Git | Github | Heroku | Notion | Trello
       </div>
       <div className="sections four">
         <h2>Production Strategy</h2>
