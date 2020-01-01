1d76bbde52a0814a74f2055ab3745fb8ceea16cc
Leslie Thompson <lcrthompson@gmail.com>
2020-01-01 13:31:19 -0500

    project updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,13 +26,7 @@ function Projects() {
     <div>
       <div className="projects-container">
         <h1 className="header">{"{ my work }"}</h1>
-        {/* <ProjectCard2 />
-      <ProjectCard
-        title="Replate"
-        titleColor="replateTitle"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        descriptionColor="replateDescription"
-      /> */}
+
         {data.map(project => (
           <ProjectCard2
             key={project.id}
