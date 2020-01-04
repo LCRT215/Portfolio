6e8f517e018862d1b685885f7143e927b55f66c6
Leslie Thompson <lcrthompson@gmail.com>
2020-01-03 23:05:51 -0500

    Gigi's suggestions

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -5,8 +5,7 @@ export default [
     titleFont: "firefly",
     description:
       "Firefly is a proof-of-concept application designed for how 4-6 year-old children learn - through play and experience. I functioned as a Full-Stack Developer on cross-functional team of 8 Web Developers and 2 UX Designers.We created an innovative, open-ended coding environment that encourages experimentation by using React and animated SVGs to build an intuitive learning experience, which allows children to figure things out without feeling stuck. Firefly more accurately teaches what it means to be an engineer, with responsive interactions using drag and drop blocks that can be combined to code their own customizable fireflies. This application was created to be used on a tablet.",
-    tech:
-      "React | React DnD | Firebase & Firestore | GraphQL | SQLite | Node.js",
+    tech: "React | Context | React DnD | Firebase & Firestore ",
     github: "https://github.com/projectfirefly",
     link: "https://projectfirefly.netlify.com/"
   },
@@ -25,7 +24,7 @@ export default [
     title: "Love, Leslie",
     titleFont: "loveLeslie",
     description:
-      "Love, Leslie is a mobile-ready responsive web application created to provide users with a boost of joy throughout the day. This random motivational quote generator accesses a custom made API filled with my favorite motivational quotes and allows authenticated users to add their own.",
+      "Love, Leslie is a mobile-ready responsive web application created to provide users with a boost of joy throughout the day. This random motivational quote generator accesses a custom made API filled with my favorite motivational quotes and allows authenticated users to add their own. (In progress)",
     tech: "React | CSS | Oauth | Node",
     link: "https://condescending-goodall-d76084.netlify.com/",
     github: "https://github.com/LCRT215/Project-Motivation"
