800a7c6e16e308eadc86ed8417b38fb6a87266c3
Leslie Thompson <lcrthompson@gmail.com>
2020-01-23 09:14:32 -0500

    Project imgs to gifs, AtoZ link to backend

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Projects.scss";
-// import pic from "./ProjectImages/replate.jpg";
-import replate from "../Projects/ProjectImages/replate.png";
-import firefly from "../Projects/ProjectImages/firefly.png";
+import replate from "../Projects/ProjectImages/replate.gif";
+import firefly from "../Projects/ProjectImages/firefly.gif";
 function ProjectCard2(props) {
   let projectImage;
   if (props.titleFont === "replate") {
@@ -10,44 +9,12 @@ function ProjectCard2(props) {
   } else if (props.titleFont === "firefly") {
     projectImage = <img className="projectImage" src={firefly} alt="Logo" />;
   }
-  // var loginButton;
-  // if (loggedIn) {
-  //   loginButton = <LogoutButton />;
-  // } else {
-  //   loginButton = <LoginButton />;
-  // }
-
-  // return (
-  //   <nav>
-  //     <Home />
-  //     {loginButton}
-  //   </nav>
-  // );
 
   return (
     <div className="cardContainer">
-      {/* <img src={require(path)} alt="project" height="600" width="600" /> */}
-      {/* <div
-        style={{ backgroundImage: `url(require("props.img"))` }}
-        className="projectImg"
-      ></div> */}
-      {/* <img src={require(props.img)} alt="not working" /> */}
-      {/* <img src={pic} className="projectImg" /> */}
-      {/* <img src={pics.replate} alt="Logo" /> */}
-      {/* <img src={require(`../${props.img}`)} /> */}
-      {/* <div style={"background-image: url('./ProjectImages/replate.jpg)"} /> */}
-
       <div className="textContainer">
-        {/* console.log(JSON.parse(myObjStr)); */}
-
-        {/* {props.titleFont === "replate" ? (
-          <img className="" src={replate} alt="Logo" />
-        ) : (
-          "no goooooo"
-        )} */}
-        {/* <img src={replate} alt="Logo" /> */}
-        {projectImage}
         <h3 className={`title ${props.titleFont}`}> {props.title} </h3>
+        {projectImage}
         <p className="description">{props.description} </p>
         <div className="techContainer">
           <h5 className="techTitle">Technologies</h5>
